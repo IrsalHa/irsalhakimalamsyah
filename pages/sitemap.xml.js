567d8c0b60a5cb0afab1d92d5722991c26bc4f68
irsal <irsal@irsals-MacBook-Pro.local>
2021-12-03 08:35:39 +0700

    fix typo baseurl

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -22,13 +22,14 @@ export async function getServerSideProps({res}) {
         "_error.js",
         "sitemap.xml.js",
         "api",
-        "404.html"
+        "404.html",
+        "500.html"
       ].includes(staticPage);
     })
     .map((staticPagePath) => {
       return `${baseUrl}/${staticPagePath}`;
     });
-    staticPages.push(baseUrl)
+    staticPages.push(baseUrl+"/")
     console.log(staticPages)
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -38,7 +39,7 @@ export async function getServerSideProps({res}) {
             <url>
               <loc>${url.search("index") != -1 ? baseUrl : url}</loc>
               <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>daily</changefreq>
+              <changefreq>NEVER</changefreq>
               <priority>1.0</priority>
             </url>
           `;
